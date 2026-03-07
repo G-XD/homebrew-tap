1dc4a2cf2bf2394570756501a10d36b65650d188
gexiangdong <xiangdong.ge@pandasofcaribbean.com>
2026-03-07 18:27:32 +0800

    feat: update Aink formula to version 0.0.5

--- Formula/aink.rb
+++ Formula/aink.rb
@@ -1,21 +1,21 @@
 class Aink < Formula
     desc "CLI to track and analyze AI coding tool usage"
     homepage "https://github.com/g-xd/aink"
-    version "0.0.4"
+    version "0.0.5"
   
     on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/g-xd/aink/releases/download/v#{version}/aink-macos-aarch64.tar.gz"
-        sha256 "4cac3e6111fe6cc64f7969ab083f02f62ffb14ded291a40a9a70b06da02f0659"
+        sha256 "b4a937eb46f16f192c9b213f9ee31361cbf86bb0708ecf114bfb99fb7d28c0b0"
       else
         url "https://github.com/g-xd/aink/releases/download/v#{version}/aink-macos-x86_64.tar.gz"
-        sha256 "a33b4891ec9e284a87a42ad9c2d9df786981e0c2913796c672c115b74f6b82e3"
+        sha256 "4f4860d6f69795db8621fcf036284f4c1deee8bf774643f1c3bf31ee6de13851"
       end
     end
   
     on_linux do
         url "https://github.com/g-xd/aink/releases/download/v#{version}/aink-linux-x86_64.tar.gz"
-        sha256 "65944105744b321665238354a489dbb2899706ca74f50c87b21738c74534b4b4"
+        sha256 "73535a5057c0f1e51710ba4c1a31f111ace586fef4c920aac3c501b205d50476"
     end
   
     def install
